e13d8ac403345cc389e58acb87def7a7929aad38
Novel_txq <439948598@qq.com>
2022-07-17 17:12:29 +0800

    静态数码管

---
+++ 4-1 静态数码管显示/main.c
@@ -0,0 +1,21 @@
+#include <REGX52.H>
+void Nixie(unsigned char Location, Number)
+{
+	switch (Location)
+	{
+	case 1:P2_4 = 1;
+			P2_3 = 1;
+	}
+
+}
+void main()
+{
+
+	while(1)
+	{
+	
+	}
+
+
+
+}
